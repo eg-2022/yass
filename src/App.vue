af142d86784d518f6ac7046d0ff16c95302d1720
eg-2022 <eduardo.garay.2022@gmail.com>
2026-07-20 23:59:15 -0600

    ADD: Placeholder App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,18 @@
 <template>
   <h1>Yass</h1>
   <p>Hello world!</p>
+  <a href="#">a tag test</a>
+  <div class="box">
+    <ol>
+      <li>List</li>
+      <li>Test</li>
+      <li>Example</li>
+    </ol>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.box {
+  margin: 2.5rem;
+}
+</style>
