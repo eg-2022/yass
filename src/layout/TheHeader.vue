f5e92b67e481585d6ebec678bb1504873a4ea76b
eg-2022 <eduardo.garay.2022@gmail.com>
2026-07-22 02:05:38 -0600

    ADD: Header layout component

---
+++ src/layout/TheHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import NavbarButton from '@/components/NavbarButton.vue'
+import TheLogo from '@/components/TheLogo.vue'
+</script>
+
+<template>
+  <header class="header">
+    <TheLogo />
+    <NavbarButton />
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: var(--background-color);
+  min-height: 5.5rem;
+  padding: 1.25rem;
+}
+</style>
