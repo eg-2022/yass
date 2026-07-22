811b00b00c2cea01b292ef2857ee3bf3da9df223
eg-2022 <eduardo.garay.2022@gmail.com>
2026-07-22 03:28:25 -0600

    ADD: Created footer layout component

---
+++ src/layout/footer/TheFooter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer">
+    <p>&#127279; 2026 - No copyright!</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  background-color: var(--the-footer-background);
+  color: var(--the-footer-color);
+  min-height: 10dvh;
+
+  & > * {
+    padding: 1rem;
+  }
+}
+</style>
