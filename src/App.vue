8406b3fa4e1b2f380982ea409c4ff8451a231f45
eg-2022 <eduardo.garay.2022@gmail.com>
2026-07-22 03:29:13 -0600

    ADD: Imported footer to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheHeader from '@/layout/header/TheHeader.vue'
+import TheFooter from './layout/footer/TheFooter.vue'
 </script>
 
 <template>
   <TheHeader />
+  <TheFooter />
 </template>
 
 <style scoped></style>
