a94dbe3491f46af2d6a314f77c169317138fd3ad
E. Garay <eduardo.garay.2022@gmail.com>
2026-07-22 18:16:05 -0600

    Merge devel branch to main #2 @ (22-jul-2026) (#2)
    * ADD: App container makes the main container to take the missing space
    
    * ADD: Main component defined
    
    * ADD: Main component import added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import TheHeader from '@/layout/header/TheHeader.vue'
-import TheFooter from './layout/footer/TheFooter.vue'
+import TheFooter from '@/layout/footer/TheFooter.vue'
+import TheMain from '@/layout/main/TheMain.vue'
 </script>
 
 <template>
   <TheHeader />
+  <TheMain />
   <TheFooter />
 </template>
 
